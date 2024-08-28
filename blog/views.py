5f2dee79cb363d0d907656b4ea20922dbe867cd2
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-08-28 22:26:22 +0300

    Use get_object_or_404

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
-from django.db.models import Count
-
+from django.shortcuts import get_object_or_404
 
 
 def serialize_post(post):
@@ -43,7 +42,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
+    post = get_object_or_404(Post, slug=slug)
     comments = Comment.objects.filter(post=post).prefetch_related('author')
     serialized_comments = []
     for comment in comments:
@@ -84,7 +83,7 @@ def post_detail(request, slug):
 
 
 def tag_filter(request, tag_title):
-    tag = Tag.objects.get(title=tag_title)
+    tag = get_object_or_404(Tag, title=tag_title)
 
     most_popular_tags = Tag.objects.popular()[:5]
 
