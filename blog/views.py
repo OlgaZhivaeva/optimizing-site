50188f7261680017dd14a6f9455a0900e40a42e5
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-08-29 20:29:18 +0300

    Fix likes_amount

--- blog/views.py
+++ blog/views.py
@@ -43,7 +43,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug)
+    post = get_object_or_404(Post.objects.popular(), slug=slug)
     comments = post.comments.all().select_related('author')
     serialized_comments = []
     for comment in comments:
@@ -60,7 +60,7 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': post.likes.count(),
+        'likes_amount': post.num_likes,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
